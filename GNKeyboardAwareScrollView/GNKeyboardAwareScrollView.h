196c8b147396c9e3bd118616870db7f9d75e61c2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2014-09-04 02:25:19 +0200

    Initial Commit

---
+++ GNKeyboardAwareScrollView/GNKeyboardAwareScrollView.h
@@ -0,0 +1,13 @@
+//
+//  GNKeyboardAwareScrollView.h
+//  GNKeyboardAwareScrollView
+//
+//  Created by Jakub Knejzlik on 04/09/14.
+//  Copyright (c) 2014 Jakub Knejzlik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNKeyboardAwareScrollView : NSObject
+
+@end
---
+++ GNKeyboardAwareScrollView/GNKeyboardAwareScrollView.m
@@ -0,0 +1,13 @@
+//
+//  GNKeyboardAwareScrollView.m
+//  GNKeyboardAwareScrollView
+//
+//  Created by Jakub Knejzlik on 04/09/14.
+//  Copyright (c) 2014 Jakub Knejzlik. All rights reserved.
+//
+
+#import "GNKeyboardAwareScrollView.h"
+
+@implementation GNKeyboardAwareScrollView
+
+@end
