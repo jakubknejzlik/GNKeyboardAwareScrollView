7ecdb9e10d3fc4d0e1ad08a1e1075fb86a437af3
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2014-09-11 20:21:58 +0200

    category for UIScrollView fix, estimatedContentInset variable added

--- GNKeyboardAwareScrollView/GNKeyboardAwareScrollView.h
+++ GNKeyboardAwareScrollView/GNKeyboardAwareScrollView.h
@@ -10,6 +10,8 @@
 
 @interface GNKeyboardAwareScrollView : UIScrollView
 
+@property (nonatomic,readonly) UIEdgeInsets estimatedContentInset;
+
 @end
 
 
--- GNKeyboardAwareScrollView/GNKeyboardAwareScrollView.m
+++ GNKeyboardAwareScrollView/GNKeyboardAwareScrollView.m
@@ -8,7 +8,13 @@
 
 #import "GNKeyboardAwareScrollView.h"
 
+@interface UIScrollView (KeyboardAwarePrivate)
+-(void)gn_updateKeyboardContentInset:(UIEdgeInsets)contentInset;
+@end
+
+
 @implementation GNKeyboardAwareScrollView
+@synthesize estimatedContentInset = _estimatedContentInset;
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -33,16 +39,20 @@
 -(void)initialize{
     [self attachKeyboardObservation];
 }
+-(void)gn_updateKeyboardContentInset:(UIEdgeInsets)contentInset{
+    [super gn_updateKeyboardContentInset:contentInset];
+    _estimatedContentInset = contentInset;
+}
 
 
 @end
 
 
-@implementation GNKeyboardAwareScrollView (KeyboardAware)
+@implementation UIScrollView (KeyboardAware)
 
 -(void)attachKeyboardObservation{
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardFrameChanged:) name:UIKeyboardWillHideNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardFrameChanged:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(gn_keyboardFrameChanged:) name:UIKeyboardWillHideNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(gn_keyboardFrameChanged:) name:UIKeyboardDidShowNotification object:nil];
 }
 
 -(void)detachKeyboardObservation{
@@ -50,11 +60,14 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
 
--(void)keyboardFrameChanged:(NSNotification *)notification{
+-(void)gn_keyboardFrameChanged:(NSNotification *)notification{
     CGRect rect = [[notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
     rect = [self.superview convertRect:rect fromView:[[[UIApplication sharedApplication] delegate] window]];
-    [self setContentInset:UIEdgeInsetsMake(0, 0, MAX(self.frame.size.height-rect.origin.y,0), 0)];
-    [self setScrollIndicatorInsets:self.contentInset];
+    [self gn_updateKeyboardContentInset:UIEdgeInsetsMake(0, 0, MAX(self.frame.size.height-rect.origin.y,0), 0)];
+}
+-(void)gn_updateKeyboardContentInset:(UIEdgeInsets)contentInset{
+    [self setContentInset:contentInset];
+    [self setScrollIndicatorInsets:contentInset];
 }
 
 @end
